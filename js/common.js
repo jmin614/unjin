ff6c8274f1005e4e1261c67cd72161cb8b105b66
jmin614 <jmin614@naver.com>
2020-12-16 12:52:38 +0900

    3_제이쿼리

--- js/common.js
+++ js/common.js
@@ -1,3 +1,8 @@
 $(function(){
-  //script 입력영역
+  $(".look_list a").click(function(){ //a를 클릭했을 때 실행
+    $("a").removeClass("on");
+    $(this).addClass("on"); //선택하기!!!
+    $(".img_box img").attr("src",$(this).attr("href")); //클릭 시 이미지 바꾸기!
+    return false;
+  });
 });
